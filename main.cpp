a9b1c1eb05f9523ee5ef8881fc7136f39bceeb7e
Caleb Brock <calebbrock19@gmail.com>
2026-03-15 13:47:23 +0800

    imlemented a switch to care for the special characters. probably a more efficient way to do it later

--- main.cpp
+++ main.cpp
@@ -4,7 +4,34 @@
 void readKey() {
 	int key = _getch();
 
-	std::cout << char(key);
+
+	// special keys 
+	if (key == 0 || key == 224) {
+		int specialKey = _getch();
+
+		switch (specialKey) {
+		case 72: 
+			std::cout << "[up]" << '\n'; 
+			break;
+		case 80: 
+			std::cout << "[DOWN]" << '\n'; 
+			break;
+		case 75: 
+			std::cout << "[LEFT]" << '\n';
+			break;
+		case 77:
+			std::cout << "[RIGHT]" << '\n';
+			break;
+		// need to add cases for F keys for more clarity later 
+		default:
+			std::cout << " SP: [" << specialKey << '] ' << '\n';
+			break;
+		}
+	}
+	else {
+		_putch(key); // used to show each output without buffering 
+	}
+
 
 }
 
